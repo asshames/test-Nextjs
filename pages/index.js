583459466889475d299be888dbfc7244974f41c0
asshames <assamialomarshames@gmail.com>
2023-02-06 21:01:56 +0600

    first part done

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,36 @@
+
 function Gymbaran () {
   return(
     <div>
-      <div>Gymbaran</div>
       <div>
         <ul>
+          <li>Gymbaran</li>
           <li>Home</li>
           <li>Program</li>
           <li>Blog</li>
           <li>About Us</li>
           <li>Log in</li>
         </ul>
+      </div>
+      <div>
+        <div>
+          <div><h1>Healthy in side <br/> fresh out side</h1></div>
+          <div><p>Exercise is a very important need for our body. Heath and fitness will be obtained if you can do regular exercise and run a healthy routine.</p></div>
+          <div>
+            <button>Get started</button>
+            <button>Learn more</button>
+          </div>
+          <div>
+            <p>Brands:</p>
+            <div>
+            <img src="n.png" alt="Nike" width="50" height="50"/>
+            <img src="a.png" alt="Adidas" width="50" height="40"/>
+            <img src="p.png" alt="Puma" width="50" height="50"/>
+            <img src="r.png" alt="Reebok" width="50" height="40"/>
+            </div>
+          </div>
+        </div>
+        <div><img src="img1.jpg" alt="Gym1" width="400" height="250"/></div>
       </div>
     </div>
   )
