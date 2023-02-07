53be4fa6314d8e8d938a3e4621a7f036ba30cace
asshames <assamialomarshames@gmail.com>
2023-02-08 02:16:05 +0600

    first and second part done with tailwind css

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,80 @@
 
 function Gymbaran () {
+
   return(
-    <div>
-      <nav>
-        <div>
-          <ul>
-            <li>Gymbaran</li>
-            <li>Home</li>
-            <li>Program</li>
-            <li>Blog</li>
-            <li>About Us</li>
-            <li>Log in</li>
+    <div className="h-full mt-4 ml-8 mr-8 p-2 bg-white">
+      <nav className="flex justify-between m-2">
+        <div className="mt-2 text-center">
+          <h1 className="">
+            <a className="" href="/"><span className="px-1 py-1 rounded-lg bg-purple-800 text-white">Gym</span><span className="text-purple-800">baran</span> </a>
+          </h1>
+        </div>
+        <div className="flex">
+          <ul className="flex m-2 text-center text-black">
+            <li className="">
+              <a href="/" className="m-4">
+                <span>Home</span>
+              </a>
+            </li>
+            <li>
+              <a href="/" className="m-4">
+                <span>Program</span>
+              </a>
+            </li>
+            <li>
+              <a href="/"className="m-4">
+                <span>Blog</span>
+              </a>
+            </li>
+            <li>
+              <a href="/" className="m-4">
+              <span className="">About Us</span>
+              </a>
+            </li>
           </ul>
+          <div>
+            <button className="px-6 py-2 text-center border border-blue-50 bg-blue-800 rounded-lg text-white">Log in</button>
+          </div>
         </div>
       </nav>
-      <div>
-        <div>
-          <div><h1>Healthy in side <br/> fresh out side</h1></div>
-          <div><p>Exercise is a very important need for our body. Heath and fitness will be obtained if you can do regular exercise and run a healthy routine.</p></div>
+      <div className="flex mt-16">
+        <div className="mt-8">
           <div>
-            <button>Get started</button>
-            <button>Learn more</button>
+            <h1 className="font-bold text-5xl">
+              Healthy in side <br/> <span className="text-purple-800">fresh</span> out side
+            </h1>
           </div>
-          <div>
+          <div className="text-justify mt-6 mr-16">
+            <p className="text-xs">Exercise is a very important need for our body. Heath and fitness will be obtained if you can do regular exercise and run a healthy routine.</p></div>
+          <div className="mt-4 space-x-5">
+            <button className="px-6 py-2 text-center border border-blue-50 bg-blue-800 rounded-lg text-white">Get started</button>
+            <button className="px-6 py-2 text-center border border-blue-50 bg-gray-200 rounded-lg text-blue-800">Learn more</button>
+          </div>
+          <div className="mt-6">
             <p>Brands:</p>
-            <div>
-            <img src="img/n.png" alt="Nike" width="50" height="50"/>
-            <img src="img/a.png" alt="Adidas" width="50" height="40"/>
-            <img src="img/p.png" alt="Puma" width="50" height="50"/>
-            <img src="img/r.png" alt="Reebok" width="50" height="40"/>
+            <div className="flex space-x-7">
+              <img className="" src="img/n.png" alt="Nike" width="50" height="50"/>
+              <img className="ml-2 mt-2 p-1 bg-white " src="img/a.png" alt="Adidas" width="50" height="40"/>
+              <img className="ml-2" src="img/p.png" alt="Puma" width="50" height="50"/>
+              <img className="ml-2" src="img/r.png" alt="Reebok" width="50" height="40"/>
             </div>
           </div>
         </div>
-        <div><img src="img/img1.jpg" alt="Gym1" width="400" height="250"/></div>
+        <div>
+          <img className="h-full rounded-md" src="img/img1.jpg" alt="Gym1"/>
+        </div>
       </div>
-      <div>
-        <div><h1>Healthy in side <br/> fresh out side</h1></div>
-        <div><p>Exercise is a very important need for our body. Heath and fitness will be obtained if you can do regular exercise and run a healthy routine. Even at home we still have to be diligent in exercise, either alone or with your friends at home. Healthy life makes you more excited to live the day.</p></div>
+      <div className="mt-16 p-2 grid grid-cols-2">
+        <div>
+          <h1 className="font-medium text-5xl">
+            Healthy in side <br/> fresh out side
+          </h1>
+        </div>
+        <div>
+          <p className="mt-6 text-xs">
+            Exercise is a very important need for our body. Heath and fitness will be obtained if you can do regular exercise and run a healthy routine. Even at home we still have to be diligent in exercise, either alone or with your friends at home. Healthy life makes you more excited to live the day.
+          </p>
+        </div>
       </div>
       <div>
         <div>
