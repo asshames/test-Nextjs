c5640f2c47de2a454725512a8665e9b028f0a8b1
asshames <assamialomarshames@gmail.com>
2023-02-08 08:58:55 +0600

    fixing style

--- pages/index.js
+++ pages/index.js
@@ -1,60 +1,70 @@
 function Gymbaran () {
   return(
-    <div className="h-full mt-4 ml-8 mr-8 p-2 bg-white">
+    <div className="h-full mt-6 ml-10 mr-10 p-4 bg-white">
       <nav className="flex justify-between m-2">
-        <div className="mt-2 text-center">
-          <h1 className="">
-            <a className="" href="/"><span className="px-1 py-1 rounded-lg bg-purple-800 text-white">Gym</span><span className="text-purple-800">baran</span> </a>
+        <div className="mt-6 text-center">
+          <h1 className="text-2xl">
+            <span className="px-1 py-1 rounded-lg bg-purple-800 text-white">Gym</span><span className="text-purple-800">baran</span>
           </h1>
         </div>
-        <div className="flex">
+        <div className="flex text-2xl">
           <ul className="flex m-2 text-center text-black">
             <li className="">
-              <a href="/" className="m-4">
+              <div className="m-4">
                 <span>Home</span>
-              </a>
+              </div>
             </li>
             <li>
-              <a href="/" className="m-4">
+              <div className="m-4">
                 <span>Program</span>
-              </a>
+              </div>
             </li>
             <li>
-              <a href="/"className="m-4">
+              <div className="m-4">
                 <span>Blog</span>
-              </a>
+              </div>
             </li>
             <li>
-              <a href="/" className="m-4">
+              <div className="m-4">
               <span className="">About Us</span>
-              </a>
+              </div>
             </li>
           </ul>
           <div>
-            <button className="px-6 py-2 text-center border border-blue-50 bg-blue-800 rounded-lg text-white">Log in</button>
+            <button className="mt-4 px-6 py-2 text-center border border-blue-50 bg-blue-800 rounded-lg text-white">Log in</button>
           </div>
         </div>
       </nav>
       <div className="flex mt-16">
-        <div className="mt-8">
+        <div className="mt-10">
           <div>
-            <h1 className="font-bold text-5xl">
+            <h1 className="font-bold text-8xl">
               Healthy in side <br/> <span className="text-purple-800">fresh</span> out side
             </h1>
           </div>
-          <div className="text-justify mt-6 mr-16">
-            <p className="text-xs">Exercise is a very important need for our body. Heath and fitness will be obtained if you can do regular exercise and run a healthy routine.</p></div>
-          <div className="mt-4 space-x-5">
-            <button className="px-6 py-2 text-center border border-blue-50 bg-blue-800 rounded-lg text-white">Get started</button>
-            <button className="px-6 py-2 text-center border border-blue-50 bg-gray-200 rounded-lg text-blue-800">Learn more</button>
+          <div className="text-justify mt-8 mr-64">
+            <p className="text-lg text-gray-500 text-justify">Exercise is a very important need for our body. Heath and fitness will be obtained if you can do regular exercise and run a healthy routine.</p></div>
+          <div className="mt-8 flex">
+            <div className="mr-10 px-20 py-5 flex items-center border border-blue-50 bg-blue-800 rounded-lg">
+              <button className="mr-16 text-center text-lg text-white">Get started</button>
+              <svg className="h-6 w-6 p-1 font-semibold border border-blue-800 rounded-full bg-white text-blue-800" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+              </svg>
+            </div>
+            <div className="mr-5 px-4 flex items-center border border-blue-50 bg-gray-200 rounded-lg">
+              <svg className="h-6 w-6 p-1 border border-white bg-blue-800 text-white rounded-full" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                <path fill-rule="evenodd" d="M4.5 5.653c0-1.426 1.529-2.33 2.779-1.643l11.54 6.348c1.295.712 1.295 2.573 0 3.285L7.28 19.991c-1.25.687-2.779-.217-2.779-1.643V5.653z" clip-rule="evenodd" />
+              </svg>
+              <button className="px-6 py-2 text-center text-lg  text-blue-800">Learn more</button> 
+            </div>
           </div>
           <div className="mt-6">
-            <p>Brands:</p>
-            <div className="flex space-x-7">
-              <img className="" src="img/n.png" alt="Nike" width="50" height="50"/>
-              <img className="ml-2 mt-2 p-1 bg-white " src="img/a.png" alt="Adidas" width="50" height="40"/>
-              <img className="ml-2" src="img/p.png" alt="Puma" width="50" height="50"/>
-              <img className="ml-2" src="img/r.png" alt="Reebok" width="50" height="40"/>
+            <p className="text-lg">Brands:</p>
+            <div className="flex space-x-24">
+              <img className="ml-1" src="img/n.png" alt="Nike" width="80" height="80"/>
+              <img className="ml-2 mt-2 p-1 bg-white " src="img/a.png" alt="Adidas" width="80" height="40"/>
+              <img className="ml-2" src="img/p.png" alt="Puma" width="80" height="80"/>
+              <img className="ml-2" src="img/r.png" alt="Reebok" width="80" height="40"/>
             </div>
           </div>
         </div>
@@ -62,63 +72,83 @@ function Gymbaran () {
           <img className="h-full rounded-md" src="img/img1.jpg" alt="Gym1"/>
         </div>
       </div>
-      <div className="mt-16 p-2 grid grid-cols-2">
+      <div className="mt-24 p-4 grid grid-cols-2">
         <div>
-          <h1 className="text-5xl">
+          <h1 className="text-7xl">
             Healthy in side <br/> fresh out side
           </h1>
         </div>
-        <div className="mt-6 text-justify ">
-          <p className="text-xs">
+        <div className="mt-10 text-justify ">
+          <p className="text-lg text-gray-500">
             Exercise is a very important need for our body. Heath and fitness will be obtained if you can do regular exercise and run a healthy routine. Even at home we still have to be diligent in exercise, either alone or with your friends at home. Healthy life makes you more excited to live the day.
           </p>
         </div>
       </div>
-      <div className="mt-16 flex justify-between text-center px-10 py-14 border border-purple-900 bg-purple-700 rounded-lg text-white">
-        <div className="">
+      <div className="mt-24 flex justify-between px-20 py-28 border border-purple-900 bg-purple-700 rounded-3xl text-white">
+        <div className="flex justify-center items-center">
           <div>
-            {/* icon */}
+            <img className="h-32 w-32 rounded-3xl" src="img/e1.png"/>
           </div>
-          <div>
-            <p className="font-bold">Get that 11 line in 30 days</p>
-            <button>Learn more </button>
-          </div>
-        </div>
-        <div>
-          <div>
-            {/* icon */}
-          </div>
-          <div>
-            <p className="font-bold">14 Days sherd challenge </p>
-            <button>Learn more </button>
+          <div className="ml-8">
+            <p className="font-bold text-2xl">Get that 11 line <br/> in 30 days</p>
+            <div className="mt-2 flex justify-between items-center text-white">
+              <button className="text-lg text-gray-300">Learn more </button>
+              <svg className="h-4 w-4 mr-7" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                <path fill-rule="evenodd" d="M3.75 12a.75.75 0 01.75-.75h13.19l-5.47-5.47a.75.75 0 011.06-1.06l6.75 6.75a.75.75 0 010 1.06l-6.75 6.75a.75.75 0 11-1.06-1.06l5.47-5.47H4.5a.75.75 0 01-.75-.75z" clip-rule="evenodd" />
+              </svg>
+            </div>
           </div>
         </div>
-        <div>
+        <div className="flex justify-center items-center">
           <div>
-            {/* icon */}
+          <img className="h-32 w-32 rounded-3xl" src="img/e2.png"/>
           </div>
+          <div className="ml-8">
+            <p className="font-bold text-2xl">14 Days <br/>sherd challenge </p>
+            <div className="mt-2 flex justify-between items-center text-white">
+              <button className="text-lg text-gray-300">Learn more </button>
+              <svg className="h-4 w-4 mr-7" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                <path fill-rule="evenodd" d="M3.75 12a.75.75 0 01.75-.75h13.19l-5.47-5.47a.75.75 0 011.06-1.06l6.75 6.75a.75.75 0 010 1.06l-6.75 6.75a.75.75 0 11-1.06-1.06l5.47-5.47H4.5a.75.75 0 01-.75-.75z" clip-rule="evenodd" />
+              </svg>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center items-center">
           <div>
-            <p className="font-bold">Get flat belly in 30 days</p>
-            <button>Learn more </button>
+          <img className="h-32 w-32 rounded-3xl" src="img/e3.png"/>
+          </div>
+          <div className="ml-8">
+            <p className="font-bold text-2xl">Get flat belly <br/> in 30 days</p>
+            <div className="mt-2 flex justify-between items-center text-white">
+              <button className="text-lg text-gray-300">Learn more </button>
+              <svg className="h-4 w-4 mr-7" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                <path fill-rule="evenodd" d="M3.75 12a.75.75 0 01.75-.75h13.19l-5.47-5.47a.75.75 0 011.06-1.06l6.75 6.75a.75.75 0 010 1.06l-6.75 6.75a.75.75 0 11-1.06-1.06l5.47-5.47H4.5a.75.75 0 01-.75-.75z" clip-rule="evenodd" />
+              </svg>
+            </div>
           </div>
         </div>
       </div>
-      <div className="mt-16 flex justify-between">
+      <div className="mt-32 ml-16 flex justify-between">
         <div className="mr-12">
-          <img className="h-full rounded-md" src="img/img2.jpg" alt="Gym2" width="1200" />
+          <img className="h-full rounded-md" src="img/img2.jpg" alt="Gym2" width={1800} />
         </div> 
-        <div className="">
+        <div className="m-16 py-8">
           <div>
-            <h1 className="text-5xl">
+            <h1 className="text-7xl">
               Best full body <br/> workout to lose fat
             </h1>
           </div>
-          <div className="mt-6 mr-16 text-justify ">
-            <p className="text-xs">
-              Exercise is a very important need for our body. Heath and fitness will be obtained if you can do regular exercise and run a healthy routine. Even at home we still have to be diligent in exercise, either alone or with your friends at home. Healthy life makes you more excited to live the day.
+          <div className="mt-8 mr-16 text-justify ">
+            <p className="text-lg  text-gray-500">
+              Exercise is a very important need for our body. Heath and fitness will be obtained if you can do regular exercise and run a healthy routine. Even at home we still have to be diligent in exercise, either alone or with your friends at home.
             </p>
           </div>
-          <button className="mt-6 px-6 py-2 text-center border border-blue-50 bg-blue-800 rounded-lg text-white">Get started</button>
+          <div className="mt-10 w-64 flex justify-between items-center text-white border border-blue-50 bg-blue-800 rounded-lg">
+            <button className="px-8 py-6 text-center text-lg">Get started</button>
+            <svg className="h-6 w-6 mr-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+              <path fill-rule="evenodd" d="M3.75 12a.75.75 0 01.75-.75h13.19l-5.47-5.47a.75.75 0 011.06-1.06l6.75 6.75a.75.75 0 010 1.06l-6.75 6.75a.75.75 0 11-1.06-1.06l5.47-5.47H4.5a.75.75 0 01-.75-.75z" clip-rule="evenodd" />
+            </svg>
+          </div>
         </div> 
       </div>
     </div>
